0e7e516b0b8ecfc29c109aaccb2a6a6f3108747a
Joe Cheng <joe@rstudio.com>
2014-07-21 18:26:30 -0700

    Initial impl of unified bindings for static and Shiny
    I've only tested this for static so far, not with Shiny.
    This commit should be considered a starting point for
    discussion, not a final proposal.

--- R/htmlwidgets.R
+++ R/htmlwidgets.R
@@ -37,7 +37,7 @@ toHTML.htmlwidget <- function(x, defaultWidth, defaultHeight){
   
   html <- htmltools::tagList(
     widget_div(x, id = id, style = style, class = class(x)[1]),
-    widget_script(x)
+    widget_data(x, id)
   )
   
   html <- htmltools::attachDependencies(html, widget_dependencies(x))
@@ -53,8 +53,7 @@ widgetOutput <- function(x){
     html <- htmltools::tagList(
       widget_div(cx, id = outputId, class = paste0(x, "_output"), 
         style = sprintf("width:%dpx; height:%dpx", width, height)
-      ),
-      widget_script(cx)
+      )
     )
     dependencies = widget_dependencies(cx)
     htmltools::attachDependencies(html, dependencies)
@@ -91,21 +90,38 @@ widget_dependencies <- function(x){
 #' @export
 widget_dependencies.htmlwidget <- function(x){
   lib = class(x)[1]
+  jsfile = attr(x, "jsfile") %||% sprintf('%s.js', lib)
   config = attr(x, "config") %||% sprintf('%s.yaml', lib)
   package = attr(x, "package") %||% lib
-  getDependency(config, package)
+  widgetDep <- getDependency(config, package)
+  
+  # TODO: The binding JS file should really be in its own directory to prevent
+  # htmltools from picking up the entire package
+  bindingDep <- htmlDependency(paste0(lib, "-binding"), packageVersion(package),
+    system.file(package = package),
+    script = jsfile
+  )
+  
+  c(
+    list(htmlDependency("htmlwidgets", packageVersion("htmlwidgets"),
+      src = system.file("www", package="htmlwidgets"),
+      script = "htmlwidgets.js"
+    )),
+    widgetDep,
+    list(bindingDep)
+  )
+}
+
+# Generates a <script type="application/json"> tag with the JSON-encoded data,
+# to be picked up by htmlwidgets.js for static rendering.
+#' @export
+widget_data <- function(x, id, ...){
+  UseMethod('widget_data')
 }
 
 #' @export
-widget_script <- function(x, ...){
-  UseMethod('widget_script')
+widget_data.htmlwidget <- function(x, id, ...){
+  tags$script(type="application/json", `data-for` = id,
+    HTML(RJSONIO::toJSON(x))
+  )
 }
-
-#' @export
-widget_script.htmlwidget <- function(x){
-  lib = class(x)[1]
-  jsfile = attr(x, "jsfile") %||% sprintf('%s.js', lib)
-  package = attr(x, "package") %||% lib
-  getScript(x, jsfile, package)
-}
-
---
+++ inst/www/htmlwidgets.js
@@ -0,0 +1,104 @@
+(function() {
+  // If window.HTMLWidgets is already defined, then use it; otherwise create a
+  // new object. This allows preceding code to set options that affect the
+  // initialization process (though none currently exist).
+  window.HTMLWidgets = window.HTMLWidgets || {};
+  
+  // Default implementations for find and initialize
+  var defaults = {
+    find: function(scope) {
+      return scope.querySelectorAll("." + this.name);
+    },
+    initialize: function(el) {
+    }
+  };
+  
+  // Equivalent of $.extend
+  function extend(target /*, ... */) {
+    if (arguments.length == 1) {
+      return target;
+    }
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var prop in source) {
+        if (source.hasOwnProperty(prop)) {
+          target[prop] = source[prop];
+        }
+      }
+    }
+    return target;
+  }
+  
+  // Called by widget bindings to register a new type of widget. The definition
+  // object can contain the following properties:
+  // - name (required) - A string indicating the binding name, which will be
+  //   used by default as the CSS classname to look for.
+  // - initialize (optional) - A function(el) that will be called once per
+  //   widget element; if a value is returned, it will be passed as the third
+  //   value to renderValue.
+  // - renderValue (required) - A function(el, data, initValue) that will be
+  //   called with data. Static contexts will cause this to be called once per
+  //   element; Shiny apps will cause this to be called multiple times per
+  //   element, as the data changes.
+  window.HTMLWidgets.widget = function(definition) {
+    if (!definition.name) {
+      throw new Error("Widget must have a name");
+    }
+    // TODO: Verify that .name is a valid CSS classname
+    if (!definition.renderValue) {
+      throw new Error("Widget must have a renderValue function");
+    }
+    
+    // Merge defaults into the definition; don't mutate the original definition
+    definition = extend({}, definition, defaults);
+    
+    if (typeof(window.Shiny) === "undefined") {
+      // We're not in a Shiny context. Use a simple widget registration scheme.
+      window.HTMLWidgets.widgets = window.HTMLWidgets.widgets || [];
+      window.HTMLWidgets.widgets.push(definition);
+    } else {
+      // It's Shiny. Register the definition as an output binding. Right now
+      // the definition is compatible with Shiny output bindings; if this is
+      // ever not the case, an adaptor would be created here.
+      var bindingName = "htmlwidgets.outputbindings." + definition.name;
+      Shiny.outputBindings.register(definition, bindingName);
+    }
+  }
+
+  // Statically render all elements that are of this widget's class
+  function staticRender() {
+    var widgets = window.HTMLWidgets.widgets || [];
+    for (var i = 0; i < widgets.length; i++) {
+      var widget = widgets[i];
+      var matches = widget.find(document.documentElement);
+      for (var j = 0; j < matches.length; j++) {
+        var el = matches[j];
+        // TODO: Check if el is already bound
+        var initResult = widget.initialize(el);
+        
+        var scriptData = document.querySelector("script[data-for='" + el.id + "']");
+        if (scriptData) {
+          var data = JSON.parse(scriptData.textContent || scriptData.text);
+          widget.renderValue(el, data, initResult);
+        }
+      }
+    }
+  }
+
+  // If not Shiny, then render after the document finishes loading
+  if (typeof(window.Shiny) === "undefined") {
+    if (document.addEventListener) {
+      document.addEventListener("DOMContentLoaded", function() {
+        document.removeEventListener("DOMContentLoaded", arguments.callee, false);
+        staticRender();
+      }, false);
+    } else if (document.attachEvent) {
+      document.attachEvent("onreadystatechange", function() {
+        if (document.readyState === "complete") {
+          document.detachEvent("onreadystatechange", arguments.callee);
+          staticRender();
+        }
+      });
+    }
+  }
+})();
