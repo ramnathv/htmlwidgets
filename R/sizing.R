639e31732b286ef67f2c7fca57e7394a6ab6fd76
Joe Cheng <joe@rstudio.com>
2014-08-01 21:14:21 -0700

    Docs

--- R/sizing.R
+++ R/sizing.R
@@ -18,12 +18,50 @@ DEFAULT_PADDING <- 15
 #' @param knitrOptions Object representing the knitr options passed to us via 
 #'   `knit_print`. If we're not doing a `knit_print` right now, then the value 
 #'   should be `NULL`.
-#' @value A list that is guaranteed to have `width` and `height` values, each of
+#' @return A list that is guaranteed to have `width` and `height` values, each of
 #'   which is either a number or CSS unit string. If `standalone=TRUE` then the
 #'   list will also have a `runtime` value that is a list, that contains two
 #'   nested lists `viewer` and `browser`. Each of those in turn has `width`,
 #'   `height`, `padding` (between 1 and 4 numbers), and `fill` (`TRUE`/`FALSE`).
 #' @keywords internal
+#' @examples
+#' x <- list(
+#'   sizingPolicy = list(
+#'     defaultWidth = 800,
+#'     defaultHeight = 500,
+#'     padding = 15,
+#'     viewer = list(
+#'       fill = TRUE,
+#'       padding = 0
+#'     ),
+#'     browser = list(
+#'       fill = FALSE,
+#'       defaultWidth = 960,
+#'       defaultHeight = 600,
+#'       padding = 20
+#'     ),
+#'     knitr = list(
+#'       # Actually knitr$defaultWidth and knitr$defaultHeight
+#'       # are ignored if figure = TRUE
+#'       defaultWidth = 800,
+#'       defaultHeight = 600,
+#'       figure = TRUE
+#'     )
+#'   )
+#' )
+#' 
+#' # Sizing for standalone mode
+#' resolveSizing(x, x$sizingPolicy, TRUE, NULL)
+#' # Sizing for knitr
+#' resolveSizing(x, x$sizingPolicy, FALSE,
+#'   list(out.width.px = 150, out.height.px = 100))
+#' 
+#' # Explicit width/height provided by user--overrides any
+#' # default width/height
+#' x$width <- 300
+#' x$height <- 250
+#' resolveSizing(x, x$sizingPolicy, FALSE,
+#'   list(out.width.px = 150, out.height.px = 100))
 resolveSizing <- function(x, sp, standalone, knitrOptions = NULL) {  
   if (isTRUE(standalone)) {
     userSized <- !is.null(x$width) || !is.null(x$height)
