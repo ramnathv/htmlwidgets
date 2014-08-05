54ef8dbe8dc43b7d8702a56cdc9d89a9a3d427ba
JJ Allaire <jj@rstudio.org>
2014-08-05 15:53:41 -0400

    add dataframeToD3 helper function

--- inst/www/htmlwidgets.js
+++ inst/www/htmlwidgets.js
@@ -305,4 +305,29 @@
       });
     }
   }
+
+  window.HTMLWidgets.dataframeToD3 = function(df) {
+    var names = [];
+    var length;
+    for (var name in df) {
+        if (df.hasOwnProperty(name))
+            names.push(name);
+        if (typeof(df[name]) !== "object" || typeof(df[name].length) === "undefined") {
+            throw new Error("All fields must be arrays");
+        } else if (typeof(length) !== "undefined" && length !== df[name].length) {
+            throw new Error("All fields must be arrays of the same length");
+        }
+        length = df[name].length;
+    }
+    var results = [];
+    var item;
+    for (var row = 0; row < length; row++) {
+        item = {};
+        for (var col = 0; col < names.length; col++) {
+            item[names[col]] = df[names[col]][row];
+        }
+        results.push(item);
+    }
+    return results;
+  }
 })();
