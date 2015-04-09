b523be8b5a4296e4ac3698016e8a3d3162a242da
Yihui Xie <xie@yihui.name>
2015-04-08 22:19:04 -0500

    no, we don't want users to be able to customize json_verbatim

--- R/utils.R
+++ R/utils.R
@@ -2,14 +2,13 @@
 toJSON2 <- function(
   x, ...,  dataframe = "columns", null = "null", na = "null", auto_unbox = TRUE,
   digits = getOption("shiny.json.digits", 16), use_signif = TRUE, force = TRUE,
-  POSIXt = "ISO8601", UTC = TRUE, rownames = FALSE, keep_vec_names = TRUE,
-  json_verbatim = TRUE
+  POSIXt = "ISO8601", UTC = TRUE, rownames = FALSE, keep_vec_names = TRUE
 ) {
   jsonlite::toJSON(
     I(x), dataframe = dataframe, null = null, na = na, auto_unbox = auto_unbox,
     digits = digits, use_signif = use_signif, force = force, POSIXt = POSIXt,
     UTC = UTC, rownames = rownames, keep_vec_names = keep_vec_names,
-    json_verbatim = json_verbatim, ...
+    json_verbatim = TRUE, ...
   )
 }
 
