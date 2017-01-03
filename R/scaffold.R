13bd393677b1c32b2dd720cee0023ddc37d29c67
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-01-03 06:18:50 -0800

    allow installBowerPkg to work with simple widget

--- R/scaffold.R
+++ R/scaffold.R
@@ -98,7 +98,7 @@ addWidgetJS <- function(name, edit){
 #
 # This function uses bower to install a javascript package along with
 # its dependencies.
-installBowerPkg <- function(pkg){
+installBowerPkg <- function(pkg, simple = FALSE){
   # check if bower is installed
   if (findBower() == ""){
     stop(
@@ -107,13 +107,17 @@ installBowerPkg <- function(pkg){
     )
   }
   #check if we are in the root directory of a package
-  if (!file.exists('DESCRIPTION')){
+  if (!simple && !file.exists('DESCRIPTION')){
     stop("You need to be in a package directory to run this!",
-      call. = F)
+         call. = F)
   }
   # set up .bowerrc to install packages to correct directory
   if (!file.exists('.bowerrc')){
-    x = '{"directory": "inst/htmlwidgets/lib"}'
+    if (!simple){
+      x = '{"directory": "inst/htmlwidgets/lib"}'
+    } else {
+      x = '{"directory": "htmlwidgets/lib"}'
+    }
     cat(x, file = '.bowerrc')
   }
   # Install package
