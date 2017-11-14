e53abc5988523b96a553a1390dcb62511c8c73b2
Joe Cheng <joe@rstudio.com>
2017-11-14 14:50:39 -0800

    More resilient pandoc self-contained mechanism

--- R/savewidget.R
+++ R/savewidget.R
@@ -11,10 +11,12 @@
 #'   filename_files).
 #' @param background Text string giving the html background color of the widget.
 #'   Defaults to white.
+#' @param title Text to use as the title of the generated page.
 #' @param knitrOptions A list of \pkg{knitr} chunk options.
 #' @export
 saveWidget <- function(widget, file, selfcontained = TRUE, libdir = NULL,
-                       background = "white", knitrOptions = list()) {
+                       background = "white", title = class(widget)[[1]],
+                       knitrOptions = list()) {
 
   # convert to HTML tags
   html <- toHTML(widget, standalone = TRUE, knitrOptions = knitrOptions)
@@ -25,8 +27,10 @@ saveWidget <- function(widget, file, selfcontained = TRUE, libdir = NULL,
       sep = "")
   }
 
-  # save the file
-  htmltools::save_html(html, file = file, libdir = libdir, background=background)
+  # Save the file
+  # Include a title; pandoc 2.0 complains if you don't have one
+  pandoc_save_markdown(html, file = file, libdir = libdir,
+    background = background, title = title)
 
   # make it self-contained if requested
   if (selfcontained) {
