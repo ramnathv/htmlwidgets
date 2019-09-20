faef59076b9d405c6949cdab1247b83417f810f2
Carson <cpsievert1@gmail.com>
2019-09-20 16:35:10 -0500

    Invoke staticRender immediately in the on-ready handler to preserve the current order of execution
    Also add a comment so we remember why this is right for the short-term but wrong for the long-term

--- inst/www/htmlwidgets.js
+++ inst/www/htmlwidgets.js
@@ -661,13 +661,31 @@
 
   // Wait until after the document has loaded to render the widgets.
   if (shinyMode && window.jQuery) {
-    // Note: this case is here to ensure that initShiny executes
-    // before staticRender executes. Since initShiny currently
-    // registers important methods like Shiny.onInputChange, if
-    // it happens to execute after staticRender, the widget
-    // won't have access to all of Shiny's methods.
+    /*
+    / Shiny 1.4.0 bumps jQuery from 1.x to 3.x, which means jQuery's
+    / on-ready handler (i.e., $(fn)) is now asyncronous (i.e., it now
+    / really means $(setTimeout(fn)). https://jquery.com/upgrade-guide/3.0/#breaking-change-document-ready-handlers-are-now-asynchronous
+    /
+    / In order to ensure the order of execution of on-ready handlers
+    / remains consistent with how it's been in the past, a static render
+    / in Shiny is now scheduled via $().
+    /
+    / This is not a long term solution: it just preserves the current order
+    / of execution. Part of that ordering is to ensure initShiny executes
+    / _after_ staticRender, which is both right and wrong:
+    / * It's wrong because, when initShiny executes, it registers methods
+    / like Shiny.onInputChange that widget authors would expect to be available
+    / during a staticRender.
+    / * It's also 'right' because initShiny currently (as of v1.4.0) wants
+    / to execute after user code so that it can bind to any dynamically
+    / created elements.
+    /
+    / A longer term solution might be to make changes to Shiny so that
+    / these methods are available before the binding takes place, and then
+    / the ordering would be: register methods -> static render -> bind.
+    */
     window.jQuery(function() {
-      setTimeout(window.HTMLWidgets.staticRender, 1);
+      window.HTMLWidgets.staticRender();
     });
   } else if (document.addEventListener) {
     document.addEventListener("DOMContentLoaded", function() {
