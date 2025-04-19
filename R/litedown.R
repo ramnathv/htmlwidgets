936ae56ab39fb5dc323b4d6f9ae392973406bb74
Duncan Murdoch <murdoch.duncan@gmail.com>
2025-04-19 11:05:39 -0400

    Include `meta` and `head` entries.

--- R/litedown.R
+++ R/litedown.R
@@ -17,21 +17,30 @@ record_print.htmlwidget <- function(x, ...) {
   output <- knitr::knit_print(html, options = options,  ...)
   meta <- attr(output, "knit_meta")
   meta <- htmltools::resolveDependencies(meta)
-  css <- js <- character()
+  head <- css <- js <- character()
+  metatag <- list()
   for (i in seq_along(meta)) {
     dep <- meta[[i]]
-    if (!is.null(dep$stylesheet)) {
+    if (!is.null(dep$stylesheet))
       css <- c(css, file.path(dep$src$file, dep$stylesheet))
-    }
-    if (!is.null(dep$script)) {
+    if (!is.null(dep$script))
       js <- c(js, file.path(dep$src$file, dep$script))
-    }
-    if (!is.null(dep$meta) || !is.null(dep$head))
-      browser()
+    if (!is.null(dep$meta))
+      metatag <- c(metatag, list(dep$meta))
+    if (!is.null(dep$head))
+      head <- c(head, dep$head)
   }
+  metatag <- unlist(lapply(metatag, function(x)
+    paste0("<meta name=\"", htmltools::htmlEscape(names(x)), "\" content=\"",
+  htmlEscape(x), "\">")))
+  head <- c(head, metatag)
   meta <- litedown::reactor("meta")
-  meta$css <- unique(c(meta$css, css))
-  meta$js <- unique(c(meta$js, js))
+  if (length(css))
+    meta$css <- unique(c(meta$css, css))
+  if (length(js))
+    meta$js <- unique(c(meta$js, js))
+  if (length(head))
+    meta$"header-includes" <- c(meta$"header-includes", head)
   litedown::reactor(meta = meta)
   xfun::new_record(output, "asis")
 }
